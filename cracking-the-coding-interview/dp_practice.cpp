7e4fd0e88f0fa6ae992fd2dbb286246df3acefa8
jing.wang1 <jing.wang1@sihuatech.com>
2019-05-15 19:57:50 +0800

    update dp_practice.cpp

--- cracking-the-coding-interview/dp_practice.cpp
+++ cracking-the-coding-interview/dp_practice.cpp
@@ -42,6 +42,17 @@ void print_path(int path[], int n, int dst)
 	printf("\n");
 }
 
+void print_path1(int path[], int n, int dst, int src)
+{
+	printf("%d", dst);
+	int i=dst;
+	for(; i>=0  &&  path[i]>=0  &&  i!=src; i=path[i])
+		printf("<——%d", path[i]);
+	printf("<——%d", src);
+	printf("debug %d %d %d %d\n", i, path[abs(i)], src, path[src]);
+	printf("\n");
+}
+
 void print_path(int path[], int n, int dst, Graph* g)
 {
 	printf("%d", dst);
@@ -59,6 +70,11 @@ void print_path2(int path[], int n, int dst, int src, Graph* g)
 	printf("%d\n", src);
 }
 
+void printPath(int path[], int nvex)
+{
+	for(int i=0; i<nvex; ++i)
+		printf("cur path[%d]=%d\n", i, path[i]);
+}
 
 int shortestPath(Graph* g, int a, int b)
 {
@@ -148,6 +164,13 @@ void test_shortestPath()
 void randomSetS(int S[], int n);
 int randomSetM(int S[], int n);
 
+struct State{
+	int node;
+	int money;
+	State():node(-1),money(INT_MIN){}
+	State(int n, int m):node(n),money(m){}
+};
+
 // S[k]表示走过节点k需要花费的钱, M表示从开始节点a出发时有钱M。
 // 求从a到b是否有路径以及花费最小钱的最短路径
 int shortestPathMoneyLimited(Graph* g, int a, int b, int *S, int M)
@@ -159,39 +182,47 @@ int shortestPathMoneyLimited(Graph* g, int a, int b, int *S, int M)
 	// int dp[nvex][M]={INT_MAX}; // 
 	// int path[nvex]={-1}; // path[k]表示节点a到节点k的最短路径上的前一节点
 	// bool visited[nvex][M]={false};
-	int dp[nvex][M]; // 
-	int path[nvex]; // path[k]表示节点a到节点k的最短路径上的前一节点
+	int dp[nvex][M+1]; // 
+	//int path[nvex]; // path[k]表示节点a到节点k的最短路径上的前一节点
+	//int path[nvex][M+1]; // 记录某个状态的前一个节点
+	State path[nvex][M+1]; 
 	bool visited[nvex][M];
 	for(int i=0; i<nvex; ++i)
-		for(int j=0; j<M; ++j)
+		for(int j=0; j<M+1; ++j)
 		{
 			dp[i][j]=INT_MAX;
 			visited[i][j]=false;
+			//path[i][j]=-1;
+			path[i][j]=State(-1, M);
 		}
-	for(int i=0; i<nvex; ++i)
-		path[i]=-1;
+	// for(int i=0; i<nvex; ++i)
+		// path[i]=-1;
 	
 	
-	dp[a][M]=0;
-	path[a]=-1;
-	//visited[a][M]=true;
-	printf("init states ok\n");
+	//dp[a][M]=0;
+	dp[a][M-S[a]]=0; // pass node-a cost S[a] dollar
+	printf("init dp[%d][%d]=%d\n", a, M-S[a], dp[a][M-S[a]]);
+	//path[a]=-1;
+	//path[a][M-S[a]]=-1;
+
 	
 	// while(true)
-	for(int nvisited=1; nvisited<nvex*M; ++nvisited)
+	int nvisited=1;
+	for(nvisited=1; nvisited<nvex*(M+1); ++nvisited)
 	{
 		int node=-1, money=-1;
 		for(int i=0; i<nvex; ++i)
 		{
-			for(int j=0; j<M; ++j)
+			for(int j=0; j<M+1; ++j)
 			{
+				//if(dp[i][j]!=INT_MAX)
+				//	printf("found dp[%d][%d]=%d\n", i, j, dp[i][j]);
 				if(visited[i][j]==false)
-				{
-					if(dp[i][j]!=INT_MAX)
-						printf("found dp[%d][%d]=%d\n", i, j, dp[i][j]);
+				{					
 					if( (node<0  &&  dp[i][j]!=INT_MAX)  ||  (node>=0  &&  money>=0  &&  dp[i][j]<dp[node][money]))
 					{
 						node=i, money=j;
+						//printf("found dp[%d][%d]=%d\n", i, j, dp[i][j]);
 					}
 				}
 			}
@@ -200,39 +231,77 @@ int shortestPathMoneyLimited(Graph* g, int a, int b, int *S, int M)
 		{
 			printf("no adjacent node\n");
 			break;
-		}
-		
+		}		
 		visited[node][money]=true;
-		printf("choose %d-%d, %d(%d)\n", node, money, dp[node][money], path[node]);
+		printf("\tchoose min [%d][%d]=%d(%d)\n", node, money, dp[node][money], path[node]);
 		
 		for(int v=g->FirstNeighbor(node); v>=0; v=g->NextNeighbor(node, v))
 		{
-			printf("%d has neighbor %d\n", node, v);
-			if(money>S[v]  &&  dp[v][money-S[v]]>dp[node][money]+g->weight(node, v))
+			//printf("%d has neighbor %d\n", node, v);
+			//if(money>=S[v])
+			//	printf("(%d, %d+%d)\n", dp[v][money-S[v]], dp[node][money], g->weight(node, v));
+			if(money>=S[v]  &&  visited[v][money-S[v]]==false  &&  dp[v][money-S[v]]>dp[node][money]+g->weight(node, v))
 			{
 				dp[v][money-S[v]]=dp[node][money]+g->weight(node, v);
-				path[v]=node;
-				printf("set dp[%d][%d] to %d(%d)\n", v, money-S[v], dp[v][money-S[v]], node);
+				//path[v]=node;
+				//path[v][money-S[v]]=node; 
+				path[v][money-S[v]]=State(node, money); // 记录使得状态dp[v][money-S[v]]更新的前一状态dp[node][money]
+				//updatePath(v, path, dp); // 更新path[v]为使得dp[v][?]最小的node??
+				printf("\t\tset neighbor dp[%d][%d] to %d(%d)\n", v, money-S[v], dp[v][money-S[v]], node);
 			}
 		}
 	}
+	printf("nvisited=%d, of %d\n", nvisited, nvex*(M+1));
 	
 	int min=-1;
 	for(int i=0; i<M; ++i)
 		if((min<0  &&  dp[b][i]!=INT_MAX)  ||  (min>=0  &&  dp[b][i]<=dp[b][min])) // 选择剩余钱最多的最短路径
+		{
 			min=i;
+			printf("check dp[%d][%d]=%d\n", b, i, dp[b][i]);
+		}
 	if(min<0  ||  dp[b][min]==INT_MAX)
 		printf("no  path from %d to %d\n", a, b);
 	else
 	{
 		printf("the shortest path from %d to %d is %d, left money %d\n", a, b, dp[b][min], min);
 
-		print_path(path, nvex, b); // 输出a到b的最短路径
-		print_path(path, nvex, b, g);
-		print_path2(path, nvex, b, a, g);
+		// int path2[nvex];
+		// for(int i=0; i<nvex; ++i)
+		// {
+			// int min=INT_MAX;
+			// for(int j=0; j<M+1; ++j)
+			// {
+				//printf("path[%d][%d]=%d\n", i, j, path[i][j]);
+				// if(path[i][j]>0  &&  path[i][j]<min)
+					// min=path[i][j];
+			// }
+			// path2[i]= (min==INT_MAX) ?  -1  :  min;
+			//printf("path2[%d]=%d\n", i, path2[i]);
+		// }
+		// path2[a]=-1;
+		// printPath(path2, nvex); // 打印path数组
+		//print_path(path2, nvex, b);
+		//print_path(path, nvex, b); // 输出a到b的最短路径
+		//print_path1(path, nvex, b, a);
+		//print_path(path, nvex, b, g);
+		//print_path2(path, nvex, b, a, g);
+		
+		// 从状态dp[b][min]的path[b][min]向前获取完整路径
+		printf("%d", b);
+		for(State state=path[b][min]; state.node>=0; state=path[state.node][state.money])
+		{
+			printf("<——%d", state.node);
+		}
+		printf("\n");
 	}
+	
+	if(min==M-S[6]-S[4]-S[7]  &&  dp[b][min]==g->weight(6, 4)+g->weight(4, 7))
+		printf("ok\n");
 	return dp[b][min];
 }
+
+
 
 void test_shortestPathMoneyLimited()
 {
@@ -265,7 +334,7 @@ void randomSetS(int S[], int n)
 	srand(seed);
 	for(int i=0; i<n; ++i)
 	{
-		S[i]=rand()%10;
+		S[i]=rand()%5;
 		printf("S[%d]=%d\n", i, S[i]);
 	}
 }
